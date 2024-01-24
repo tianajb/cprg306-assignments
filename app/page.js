d678ca3cc1ca2bee12bbc438b38fabb1378fd87c
tianajb <kriz_bautista@yahoo.com>
2024-01-24 11:53:18 -0700

    week-3 assignment

--- app/page.js
+++ app/page.js
@@ -5,9 +5,9 @@ export default function Page(){
     <main>
       <h1 className ="text-2xl">CPRG 306: Web Development 2 - Assignments</h1>
 
-      <p>
-          <Link href="week-2">Week-2 Assignment</Link>
-      </p>
+      <p><Link href="week-2">Week-2 Assignment</Link></p>
+      <p><Link href="week-3">Week-3 Assignment</Link></p>
+
     </main>
   );
 }
