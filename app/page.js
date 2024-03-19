a823bafa34be47cb83cf9872169f2596b9440a3e
tianajb <kriz_bautista@yahoo.com>
2024-03-19 09:53:34 -0600

    week-3 lecture deletion

--- app/page.js
+++ app/page.js
@@ -11,6 +11,7 @@ export default function Page() {
       <p><Link href="week-5">Week-5 Assignment</Link></p>
       <p><Link href="week-6">Week-6 Assignment</Link></p>
       <p><Link href="week-7">Week-7 Assignment</Link></p>
+      <p><Link href="week-8">Week-8 Assignment</Link></p>
 
     </main>
   );
