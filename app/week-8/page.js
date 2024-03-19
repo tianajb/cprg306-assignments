6601ef07f4bf0cd71ac0ed8cbf67b1255359669e
tianajb <kriz_bautista@yahoo.com>
2024-03-19 13:59:50 -0600

    week-8 lab

---
+++ app/week-8/page.js
@@ -0,0 +1,56 @@
+"use client";
+//import Link from "next/link";
+import SPage from "./shopping-list/page";
+// Import the useUserAuth hook
+import { useUserAuth } from "./_utils/auth-context";
+
+
+
+export default function Page() {
+    // Use the useUserAuth hook to get the user object and the login and logout functions
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+    // Sign in to Firebase with GitHub authentication
+    const signIn = async () => {
+        await gitHubSignIn();
+
+    };
+
+    // Sign out of Firebase
+    const signOut = async () => {
+        await firebaseSignOut();
+    };
+    return (
+        <div>
+            <h1 className="text-3xl">Week 8 Assignment</h1>
+            {user ? (
+                <div>
+
+                    <div className="my-3 flex flex-row w-auto">
+                        <div className="w-5/6">
+                            <p className="text-white text-xl">Welcome, {user.displayName} ({user.email})</p>
+                        </div>
+                        <div className=""><button id="sign out"
+                            onClick={() => signOut()}
+                            className="bg-green-600 active:bg-green-300
+                            size-10 w-32 m-1 rounded-md">Sign out</button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <SPage />
+                    </div>
+                </div>
+            ) :
+                (
+                    <div className="my-3">
+                        <p>Welcome, Please sign in</p>
+                        <button id="sign in"
+                            onClick={() => signIn()}
+                            className="bg-green-600 active:bg-green-300
+                            size-10 w-32 m-1 rounded-md">Sign in</button>
+                    </div >
+                )}
+        </div>
+    );
+}
